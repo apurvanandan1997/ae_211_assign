3b56aeb83288c06aa588c17137c7b8481e23077e
Apurva Nandan <apurva.nandan4@gmail.com>
2020-02-15 02:51:08 +0530

    Second commit

--- ae211.m
+++ ae211.m
@@ -1,50 +1,71 @@
-x1=(-19.5:.5:-8.0);
-y1=zeros(1,24);
- plot(x1,y1,'k')
- hold on
- x2=(-8:-2)
- y2=-3*sqrt(64 - x2.^2)
- plot(x2,y2,'k')
-  x3=(-2:-1);
-  y3=(-15 *(x3+1) - 10);
-  plot(x3,y3,'k')
-   x4=(-1:1);
-   y4=(-(abs(x4) - 1) - 10);
-   plot(x4,y4,'k')
-   x5=(1:2);
-   y5=(15*(x5 - 1) - 10);
-   plot(x5,y5,'k')
-    x6=(2:8)
-    y6=-3*sqrt(64 - x6.^2)
-    plot(x6,y6,'k')
-      x7=(8:.5:19.5)
-      y7=-3*sqrt(64 - x7.^2)
-      plot(x7,y7,'k')
-x8=(-19.5:.5:-8.0);
-y8=-.5 *(.9*x8 + 17.54).^2+(5e-05);
- plot(x8,y8,'k')
- x9=(-8:0)
- y9=-.6*(1.1*x9 + 7.5).^2 - 52 -0.4438+(5e-05)
- plot(x9,y9,'k')
-  x10=(0:8);
-  y10=-.6*(-1.1*x10 + 7.5).^2 - 52-0.4438+(5e-05);
-  plot(x10,y10,'k')
-   x11=(8:19.5);
-   y11=-.5*(-.9*x11 + 17.54).^2+(5e-05);
-   plot(x11,y11,'k')
-   x12=(1:2);
-   y12=(15*(x12 - 1) - 10)+(5e-05);
-   plot(x12,y12,'k')
-    x13=(2:8)
-    y13=-3*sqrt(64 - x13.^2)+(5e-05);
-    plot(x13,y13,'k')
-      x14=(8:.5:19.5)
-      y14=-3*sqrt(64 - x14.^2)+(5e-05)
-      plot(x14,y14,'k')
-       x15=-2.*ones(1,2848)
-      y15=( -3*sqrt(60):0.01:5+(3*sqrt(60)-floor(3*sqrt(60))))
-      plot(x15,y15,'k')
-       x16=2.*ones(1,2848)
-      y16=(-3*sqrt(60):0.01:5+(3*sqrt(60)-floor(3*sqrt(60))))
-      plot(x16,y16,'k')
-  hold off
+%Source Panel Method
+%Example 3.19 of Fundamentals of Aerodynamics by J.D. Anderson
+%Code by: Dr. Bilal A. Siddiqui,
+%Asst. Prof., Mechanical Engineering, DHA Suffa University
+clc; clear all;
+syms y(x)
+
+y(x) = piecewise((-17.54/0.9)<=x & x<=-8 , -0.5 *(.9*x + 17.54).^2, -8<x & x<0, -0.6*(1.1*x + 7.5).^2 - 52.4438, 0<=x & x<8,-0.6*(-1.1*x + 7.5).^2 - 52.4438, (8<=x & x<=17.54/0.9), -0.5*(-.9*x + 17.54).^2);
+
+Vinf=30;
+n=4;%number of panels
+dx=2*17.54/(0.9*n);
+alfa=0;%angle of attack;
+X=-17.54/0.9:dx:17.54/0.9;
+Y=[];
+for itr=1:length(X)
+    Y(itr)=y(X(itr));
+end
+for index=1:n
+    %angle of flow with tangent of panel
+    phi(index)=-alfa+...
+        atan2((Y(index+1)-Y(index)),(X(index+1)-X(index)));
+    %angle of flow with normal of panel
+    beta(index)=phi(index)+pi/2;
+    midpoint_x(index)=(X(index+1)+X(index))/2;
+    midpoint_y(index)=(Y(index+1)+Y(index))/2;
+    S(index)=sqrt((Y(index+1)-Y(index))^2+...
+        (X(index+1)-X(index))^2);%length of panel
+end
+%The Source Panel Method
+for p=1:n
+    neighbors(:,p)=[1:p-1 p+1:n];
+    xi=midpoint_x(p);
+    yi=midpoint_y(p);
+    for index=1:n-1
+        m=neighbors(index,p);
+        Xj=X(m);
+        Yj=Y(m);
+        Xj1=X(m+1);
+        Yj1=Y(m+1);
+        A=-(xi-Xj)*cos(phi(m))-(yi-Yj)*sin(phi(m));
+        B=(xi-Xj)^2+(yi-Yj)^2;
+        C=sin(phi(p)-phi(m));
+        D=(yi-Yj)*cos(phi(p))-(xi-Xj)*sin(phi(p));
+        E=sqrt(B-A^2);
+        Sj=S(m);
+        I(p,m)=C/2*log((Sj^2+2*A*Sj+B)/B)+...
+            (D-A*C)/E*(atan2((Sj+A),E)-atan2(A,E));
+        J(p,m)=(D-A*C)/2/E*log((Sj^2+2*A*Sj+B)/B)...
+            -C*(atan2((Sj+A),E)-atan2(A,E));
+    end
+    F(p,1)=Vinf*cos(beta(p));
+end
+
+M=I/2/pi+eye(n)/2;
+
+lambda=-inv(M)*F;
+fprintf('The sum of all sources is %f',lambda'*S');%check sum
+% 
+% %Recoving velocity at the nodes
+% V=Vinf*sin(beta)+lambda'/2/pi*J';
+% 
+% Cp=1-(V/Vinf).^2;
+% angles=min(beta):0.01:max(beta);
+% Cp_exact=1-4*sin(angles).^2;
+% subplot(1,2,1);plot(R*cos(0:0.01:2*pi),R*sin(0:0.01:2*pi),'b',...
+%     X,Y,'r',midpoint_x,midpoint_y,'g^');axis equal;
+% legend('Exact Shape','Panel approximation','Control Points')
+% subplot(1,2,2);plot(angles,Cp_exact,'b',beta,Cp,'r^');grid;
+% legend('C_p (exact', 'C_p (Source Panel Method)');
+
